6f72c02327728f622b444282d9e810eb0d5408c4
Abrahão Martins <abrahaosrmartins@gmail.com>
2022-03-15 11:03:35 -0300

    [Learning]Class 06

---
+++ pythonteste/aula06a.py
@@ -0,0 +1,11 @@
+# SOMA
+n1 = int(input('Digite um valor: '))
+n2 = int(input('Outro: '))
+s = n1 + n2
+print('Como os valores foram typados como inteiros: A soma entre {} e {} vale {}' .format(n1, n2, s))
+
+# CONCATENAÇÃO
+n1 = input('Digite um valor: ')
+n2 = input('Outro: ')
+s = n1 + n2
+print('Como os valores foram typados como string: A concatenação entre {} e {} vale {}' .format(n1, n2, s))
