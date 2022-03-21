22b1fac477d80bcd9a6c7c6bb5c93aa95adedd9c
Abrahão Martins <abrahaosrmartins@gmail.com>
2022-03-21 19:59:00 -0300

    [Practice] Exercises 15 - 21

---
+++ Exercises/ex019.py
@@ -0,0 +1,9 @@
+# Sorteando alunos numa lista
+from random import choice
+a1 = str(input('Digite o nome do primeiro aluno: '))
+a2 = str(input('Digite o nome do segundo aluno: '))
+a3 = str(input('Digite o nome do terceiro aluno: '))
+a4 = str(input('Digite o nome do quarto aluno: '))
+lista = [a1, a2, a3, a4]
+
+print('O sorteado, foi: {}' .format(choice(lista)))
---
+++ Exercises/ex020.py
@@ -0,0 +1,10 @@
+# Sorteando alunos numa lista
+from random import shuffle
+a1 = str(input('Digite o nome do primeiro aluno: '))
+a2 = str(input('Digite o nome do segundo aluno: '))
+a3 = str(input('Digite o nome do terceiro aluno: '))
+a4 = str(input('Digite o nome do quarto aluno: '))
+lista = [a1, a2, a3, a4]
+shuffle(lista)
+
+print('A nova ordem é: {}\n' .format(lista))
