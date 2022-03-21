8e974bd28f70594f9c85c0f196da7d117b702364
Abrahão Martins <abrahaosrmartins@gmail.com>
2022-03-21 20:16:23 -0300

    [Refactor] Added line break to file according to PEP

--- pythonteste/aula07a.py
+++ pythonteste/aula07a.py
@@ -1,6 +1,6 @@
 n1 = float(input('Digite um valor: '))
 n2 = float(input('Outro'))
-sum = n1 + n2
+soma = n1 + n2
 mul = n1 * n2
 div = n1/n2
 int_div = n1//n2
@@ -8,4 +8,5 @@ rest_div = n1%n2
 exp = n1**n2
 root = n1**(1/n2)
 
-print('A soma é {}, a multiplicação é {}, a divisão é {}, a divisão inteira é {}, o resto da divisão é {}' .format(sum, mul, div, int_div, rest_div))
+print('A soma é {}, a multiplicação é {}, a divisão é {}, a divisão inteira é {}, o resto da divisão é {}'
+      .format(soma, mul, div, int_div, rest_div))
