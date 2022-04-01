5e8821d46e29effb9bf31e5f1b3bdbe4a8212dec
Abrahão Martins <abrahaosrmartins@gmail.com>
2022-04-01 18:37:36 -0300

    [Refactor] Changed conta.py
    - Added private methods
    - Added properties and setters

--- Alura/Formação_Python_para_web/Python3_part3_OO/oo/conta.py
+++ Alura/Formação_Python_para_web/Python3_part3_OO/oo/conta.py
@@ -13,17 +13,26 @@ class Conta:
     def deposita(self, valor):
         self.__saldo += valor
 
+    def __pode_sacar(self, valor_a_sacar):
+        valor_disponivel = self.saldo + self.limite
+        return valor_a_sacar <= valor_disponivel
+
     def saca(self, valor):
-        self.__saldo -= valor
+        if self.__pode_sacar(valor):
+            self.__saldo -= valor
+        else:
+            print("Você não tem saldo para esta operação")
 
     def transfere(self, valor, destino):
         self.saca(valor)
         destino.deposita(valor)
 
-    def get_saldo(self):
+    @property
+    def saldo(self):
         return self.__saldo
 
-    def get_titular(self):
+    @property
+    def titular(self):
         return self.__titular
 
     @property
