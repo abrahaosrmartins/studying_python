0f7a69dd8a11cc3ac1089174bd4d5ffc5e347c4b
Abrahão Martins <abrahaosrmartins@gmail.com>
2022-04-02 19:28:51 -0300

    [Feature] Added __str__ method

--- Alura/Formação_Python_para_web/Python3_part3_part4OO/oo2/modelo.py
+++ Alura/Formação_Python_para_web/Python3_part3_part4OO/oo2/modelo.py
@@ -19,11 +19,17 @@ class Programa:
     def nome(self, novo_nome):
         self._nome = novo_nome.title()
 
+    def __str__(self):
+        return f'Nome: {self.nome}\nAno: {self.ano}\nLikes: {self.likes}\n'
+
 
 class Filme(Programa):
     def __init__(self, nome, ano, duracao):
         super().__init__(nome, ano)
         self.duracao = duracao
+
+    def __str__(self):
+        return f'Nome: {self.nome}\nAno: {self.ano}\nDuração: {self.duracao} min\nLikes: {self.likes}\n'
 
 
 class Serie(Programa):
@@ -31,9 +37,18 @@ class Serie(Programa):
         super().__init__(nome, ano)
         self.temporadas = temporadas
 
+    def __str__(self):
+        return f'Nome: {self.nome}\nAno: {self.ano}\nTemporadas: {self.temporadas}\nLikes: {self.likes}\n'
+
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
 print(f'{vingadores.nome} - {vingadores.ano} - {vingadores.duracao} - {vingadores.likes}')
 
 atlanta = Serie('atlanta', 2018, 2)
-print(f'{atlanta.nome} - {atlanta.ano} - {atlanta.temporadas} - {atlanta.likes}')
+atlanta.dar_like()
+atlanta.dar_like()
+
+filmes_e_series = [vingadores, atlanta]
+
+for programa in filmes_e_series:
+    print(programa)
