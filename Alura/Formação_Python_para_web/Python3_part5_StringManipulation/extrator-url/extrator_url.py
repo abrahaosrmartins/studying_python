d359c023041fac0cffe84a81284e7c3032044f27
Abrahão Martins <abrahaosrmartins@gmail.com>
2022-04-03 17:27:45 -0300

    [Feature] Added sre module for string validation

--- Alura/Formação_Python_para_web/Python3_part5_StringManipulation/extrator-url/extrator_url.py
+++ Alura/Formação_Python_para_web/Python3_part5_StringManipulation/extrator-url/extrator_url.py
@@ -1,3 +1,6 @@
+import re
+
+
 class ExtratorUrl:
     def __init__(self, url):
         self.url = self.sanitiza_url(url)
@@ -13,6 +16,12 @@ class ExtratorUrl:
     def valida_url(self):
         if not self.url:
             raise ValueError("A URL está vazia")
+
+        padrao_url = re.compile('(http(s)?://)?(www.)?bytebank.com(.br)?/cambio')
+        match = padrao_url.match(self.url)
+
+        if not match:
+            raise ValueError("A URL não é válida")
 
     def get_indice_interrogacao(self):
         return self.url.find('?')  # find() retorna o indice do elemento
